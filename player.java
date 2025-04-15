eb4c5ad48fce0046f38b87fcd5edabeae82ef223
james <james89082@students.sbcsc.k12.in.us>
2025-04-15 15:23:31 -0400

    not fiinished

---
+++ player.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class player {
+    Scanner sc = new Scanner(System.in);
+    private String name;
+    private int score;
+
+    public void setName(){
+        System.out.println("what will your username be: ");
+        
+    }
+}
